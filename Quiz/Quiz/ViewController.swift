37f8c55809a1f94733e346907be9bf5a7bd70cf5
Alejandro <chapsjrl@iMac-de-Alejandro.local>
2018-02-19 21:05:40 -0600

    Adding the first version of Quiz app

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by d182_alejandro_r on 17/02/18.
+//  Copyright © 2018 alejandro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionlabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes" ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionlabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionlabel.text = questions[currentQuestionIndex]
+    }
+}
+
